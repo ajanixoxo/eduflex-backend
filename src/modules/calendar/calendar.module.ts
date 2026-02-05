cfb2198bd33084c4eeaa52f321255c52e7caabcf
Miracle Eneje <miracleeneje@Miracles-MacBook-Air.local>
2026-02-05 22:56:50 +0100

    fix: Add User model to CalendarModule for proper population

--- src/modules/calendar/calendar.module.ts
+++ src/modules/calendar/calendar.module.ts
@@ -7,12 +7,14 @@ import {
 import { CalendarService } from './calendar.service';
 import { CalendarController } from './calendar.controller';
 import { Course, CourseSchema } from '../course/schemas/course.schema';
+import { User, UserSchema } from '../user/schemas';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: CalendarSubscription.name, schema: CalendarSubscriptionSchema },
       { name: Course.name, schema: CourseSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [CalendarController],
